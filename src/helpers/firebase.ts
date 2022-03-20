1977483974c5ffccafd17b0e98481fb02739111d
Rob van Bakel <robbakel@gmail.com>
2022-03-20 19:19:40 +0100

    Log test results to Firebase

---
+++ src/helpers/firebase.ts
@@ -0,0 +1,17 @@
+require('dotenv').config()
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default getFirestore(app);
--- src/logger.ts
+++ src/logger.ts
@@ -1,11 +1,23 @@
+import { doc, updateDoc } from "firebase/firestore"; 
+import db from './helpers/firebase';
+
 import notify from './helpers/sendGrid'
+
+const saveResult = async (id: string, success: boolean) => {
+
+  const docRef = doc(db, "tests", id);
+  const data = { [Date.now()]: { success }}
+
+  updateDoc(docRef, data);
+
+}
 
 export default {
   success(id: string) {
-    console.log('✅ CHECK PASSED: ' + id)
+    saveResult(id, true);
   },
   error(id: string, data?: any) {
-    console.log('❌ CHECK FAILED: ' + id)
-    notify(id, data)
+    saveResult(id, false);
+    notify(id, data);
   },
 }

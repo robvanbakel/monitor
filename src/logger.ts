a8ea5032bb33fbf5a4d3702ee15b84c7e397d836
Rob van Bakel <robbakel@gmail.com>
2022-06-11 09:36:20 +0200

    Disable logging to Firebase

--- src/logger.ts
+++ src/logger.ts
@@ -1,14 +1,15 @@
-import { doc, updateDoc } from "firebase/firestore"; 
-import db from './helpers/firebase';
+// import { doc, updateDoc } from "firebase/firestore"; 
+// import db from './helpers/firebase';
 
 import notify from './helpers/sendGrid'
 
 const saveResult = async (id: string, success: boolean) => {
 
-  const docRef = doc(db, "tests", id);
-  const data = { [Date.now()]: { success }}
+  // const docRef = doc(db, "tests", id);
+  const data = { [new Date().toISOString()]: { success }}
 
-  updateDoc(docRef, data);
+  // updateDoc(docRef, data);
+  console.log(`${success ? '✅' : '❌'} ${id}`)
 
 }
 
